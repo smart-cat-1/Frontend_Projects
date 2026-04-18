e9cde5161f89cbcd057acea331a536194f2312f2
smart-cat-1 <jhy1750883993@gmail.com>
2026-04-18 16:53:30 +0000

    button fix

--- 03_personal_portfolio/script.js
+++ 03_personal_portfolio/script.js
@@ -1,17 +1,23 @@
-const theme = localStorage.getItem('theme');
-if (theme) {
-  document.body.setAttribute('data-theme', theme);
-} else {
-  localStorage.setItem('theme', 'dark');
-}
+const savedTheme = localStorage.getItem('theme') || 'light';
+document.body.setAttribute('data-theme', savedTheme);
+
+const sunIcon = document.getElementById('sun_icon');
+const moonIcon = document.getElementById('moon_icon');
+
+if (savedTheme === 'dark') {
+    sunIcon.style.display = 'block';
+    moonIcon.style.display = 'none';
+  } else {
+    sunIcon.style.display = 'none';
+    moonIcon.style.display = 'block';
+  }
 
 function toggleTheme() {
-  const sunIcon = document.getElementById('sun_icon');
-  const moonIcon = document.getElementById('moon_icon');
-  const body = document.body;
-  const theme = body.getAttribute('data-theme');
-  body.setAttribute('data-theme', theme === 'dark' ? 'light' : 'dark');
-  if (theme === 'dark') {
+  const currentTheme = document.body.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+   
+  document.body.setAttribute('data-theme', newTheme);
+  if (newTheme === 'dark') {
     sunIcon.style.display = 'block';
     moonIcon.style.display = 'none';
   } else {
