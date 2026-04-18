9df8cce97d4a8f3802b033b8e43bb772a54e4390
smart-cat-1 <jhy1750883993@gmail.com>
2026-04-18 17:01:41 +0000

    fix

--- 03_personal_portfolio/script.js
+++ 03_personal_portfolio/script.js
@@ -4,19 +4,23 @@ document.body.setAttribute('data-theme', savedTheme);
 const sunIcon = document.getElementById('sun_icon');
 const moonIcon = document.getElementById('moon_icon');
 
-if (savedTheme === 'dark') {
+if(sunIcon && moonIcon) {
+  if (savedTheme === 'dark') {
     sunIcon.style.display = 'block';
     moonIcon.style.display = 'none';
   } else {
     sunIcon.style.display = 'none';
     moonIcon.style.display = 'block';
   }
+}
 
 function toggleTheme() {
   const currentTheme = document.body.getAttribute('data-theme');
   const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
    
   document.body.setAttribute('data-theme', newTheme);
+  localStorage.setItem('theme', 'newTheme')
+
   if (newTheme === 'dark') {
     sunIcon.style.display = 'block';
     moonIcon.style.display = 'none';
